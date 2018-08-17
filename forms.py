d62f88ce92b5a7574704e73d0addac0a9a6d00e7
DennisWiener <grincharg@gmail.com>
2018-08-17 20:36:11 +0300

    imported branch

--- forms.py
+++ forms.py
@@ -5,14 +5,14 @@ from wtforms import StringField, SubmitField, SelectField
 class RuleForm(FlaskForm):
 
 
-    src_addr = StringField("Source Address")
-    dst_addr = StringField("Destination Address")
+    source = StringField("Source Address")
+    destination = StringField("Destination Address")
 
     src_port = StringField("Source Port")
     dst_port = StringField("Destination Port")
 
 
-    protocol = SelectField('Protocol', choices=[('any', 'any'), ('tcp', 'TCP'), ('udp', 'UDP')])
+    ipprotocol = SelectField('Protocol', choices=[('any', 'any'), ('tcp', 'TCP'), ('udp', 'UDP'), ('inet', 'INET')])
 
     interface = SelectField('Incoming interface', choices=[('lan', 'LAN'), ('wan', 'WAN')])
 
--- gui.py
+++ gui.py
@@ -32,46 +32,44 @@ def printsysinfo():
             else:
                 sys_info[sys_data.tag].append(sys_data.text)
 
-    for k, v in sys_info.items():
-        formated_str += k.upper()
-        formated_str += ': ' + str(v) + '\n'
+    # for k, v in sys_info.items():
+    #     formated_str += k.upper()
+    #     formated_str += ': ' + str(v) + '\n'
     return render_template('general_info.html', gen_info=sys_info.items())
 
 @app.route('/interfaces')
 def printinterfaceinfo():
-    int_title = '*** INTERFACES INFORMATION ***' + '\n' * 2
-    int_str = ''
+    result = {}''
     for interface in root.iterfind('./interfaces/'):
-        net = ''
+        result[interface.tag] = {}
         pref = ''
-        full_add = ''
+        net = ""
         enab = False
-        int_str += (interface.tag.upper() + '\n')
-        int_str += '*' * 10 + '\n'
         for int_config in interface.iterfind('./'):
             if int_config.tag == 'enable':
                 enab = True
             if int_config.tag == 'ipaddr':
                 net = int_config.text
+                if net == '' and pref == '':
+                    result[interface.tag][int_config.tag] = 'IP address not specified'
+                elif net == 'dhcp':
+                    result[interface.tag][int_config.tag] = 'IP address provided by DHCP'
+                else:
+                    result[interface.tag][int_config.tag] = net
             if int_config.tag == 'subnet':
                 pref = int_config.text
-        if enab:
-            int_str += 'Interface enabled' + '\n'
-        else:
-            int_str += 'WARNING: Interface disabled' + '\n'
-        if net == '' and pref == '':
-            int_str += 'IP address not specified' + '\n' * 2
-        elif net == 'dhcp':
-            int_str += 'IP address provided by DHCP' + '\n' * 2
-        else:
-            full_add = net + '/' + pref
-            sub = ipaddress.IPv4Interface(full_add)
-            int_str += ('IP Address:{} \nPrefix:{} \nSubnet:{} \nBroadcast:{}'.format(net, pref,
-                                                                                      sub.network.network_address,
-                                                                                      sub.network.broadcast_address) + '\n' * 2)
-        result = int_title + int_str
+                result[interface.tag][int_config.tag] = pref
+            if enab:
+                result[interface.tag]['status'] = 'Interface enabled'
+            else:
+                result[interface.tag]['status'] = 'WARNING: Interface disabled'
+            if net != '' and pref != '':
+                full_add = net + '/' + pref
+                sub = ipaddress.IPv4Interface(full_add)
+                result[interface.tag]['subnet'] = sub.network.network_address
+                result[interface.tag]['broadcast'] =  sub.network.broadcast_address
 
-    return render_template('interfaces.html', result=result)
+    return render_template('interfaces.html', result=result.items())
 
 
 
@@ -80,7 +78,49 @@ def RuleCheck():
     form = RuleForm()
 
     if request.method == 'POST':
-        return "Success!"
+
+        filter_dict = {}
+        for index, item in enumerate(root.iterfind('./filter/')):
+            # if item.tag == 'rule':
+            #     print(index+1, item.tag)
+            # else:
+            #     continue
+            for i in item.iterfind('./'):
+                # print(i.tag)
+                if index not in filter_dict.keys():
+                    filter_dict[index] = {}
+                    filter_dict[index][i.tag] = [i.text]
+                else:
+                    if i.tag == 'source':
+                        for x in i:
+                            filter_dict[index][i.tag] = {}
+                            filter_dict[index][i.tag] = [x.tag, x.text]
+                    elif i.tag == 'destination':
+                        for x in i:
+                            filter_dict[index][i.tag] = {}
+                            filter_dict[index][i.tag] = [x.tag, x.text]
+                    else:
+                        filter_dict[index][i.tag] = [i.text]
+        #         filter_dict[rule.tag][i.tag] = [i.text]
+
+
+
+        test = filter_dict
+
+        target_dict = {}
+        target_dict.update(request.form)
+        print(target_dict)
+        print(test)
+
+        for k, v in test.items():
+            if target_dict.items() <= v.items():
+                # print(target_dict.items(), print(v.items()))
+                print('Rule at line {} matched. Rule ID is {} \n'.format(k + 1, v['tracker']))
+                for x, y in v.items():
+                    print(x, y)
+
+        return render_template('rule_checker.html', form=form)
+
     elif request.method == 'GET':
         return render_template('rule_checker.html', form=form)
 
