bde22bdf0266cdcec04bedb8845ef3893bb528ff
Timur Asylov <asylov@gmail.com>
2018-08-18 03:35:57 +0300

    done

--- forms.py
+++ forms.py
@@ -1,16 +1,18 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField
 
+
 class RuleForm(FlaskForm):
 
-    src_addr = StringField("Source address")
-    dst_addr = StringField("Destination address")
+    source = StringField("Source Address")
+    destination = StringField("Destination Address")
 
-    src_port = StringField("Port")
-    dst_port = StringField("Port")
+    src_port = StringField("Source Port")
+    dst_port = StringField("Destination Port")
+
 
     ipprotocol = SelectField('Protocol', choices=[('any', 'any'), ('tcp', 'TCP'), ('udp', 'UDP'), ('inet', 'INET')])
-
     interface = SelectField('Incoming interface', choices=[('lan', 'LAN'), ('wan', 'WAN')])
+
 
     submit = SubmitField("Check")
