88f9b1e9b1d585e7d295c085cdefc50c86afa998
Асылов Тимур <asylov@gmail.com>
2019-11-06 09:48:53 +0300

    console log data from JSON by arr with req keys

--- src/js/views/infoView.js
+++ src/js/views/infoView.js
@@ -1,40 +1,47 @@
-import { elements } from './base';
+import {
+    elements
+} from './base';
 
 export const renderInfo = info => {
 
-    const obj = info.results;  
-    const sysKey = 'system';
-    const intKey = 'interfaces';
-    /*
-    const sysKeyArr = ['hostname', 'domain','timezone','dnsserver']
-    const intKeyArr = ['wan', 'lan']
-    */
-   
-    function getProperty (obj, key) {
-        if(obj instanceof Object) {
-        if(key in obj) return obj[key];
-        
-        for (var k in obj) {
-            var rec= findProperty(obj[k],key);
-            if(rec) return rec;
-        }
+    const interfaces = info.results.interfaces;
+    const system = info.results.system;
+
+    const sysKey = ['hostname', 'domain', 'timezone', 'dnsserver']
+  
+    // Get properties from arr in system obj
+    sysKey.forEach(function(el) {
+        console.log(`${el} ${getProperty(system, el)}`);
+      });
+
+    // Get prop from obj
+    function getProperty(obj, key) {
+        if (key in obj)
+            return obj[key];
+
         return "Cвойство отсутствует";
+    }
+
+    //  Get all keys from obj
+    function getAll(obj) {
+        for (const prop in obj) {
+            console.log(`${prop} = ${JSON.stringify(obj[(prop)])}`);
         }
-        return null;
-        }
-   
+    }
+
     const markup = `
         <div class="card-body">
-           <h2>Genetal info</h2>
-            hostname<p>${getProperty(obj, sysKey).hostname}</p>
-            domain<p>${getProperty(obj, sysKey).domain}</p>
-            timezone<p>${getProperty(obj, sysKey).timezone}</p>
-            dnsserver<p>${getProperty(obj, sysKey).dnsserver}</p>
-           <h2>Interfaces</h2>
+           <h2>General info</h2>
+           ${sysKey[0]}<p>${getProperty(system, sysKey[0])}</p>
+           ${sysKey[1]}<p>${getProperty(system, sysKey[1])}</p>
+           ${sysKey[2]}<p>${getProperty(system, sysKey[2])}</p>
+           ${sysKey[3]}<p>${getProperty(system, sysKey[3])}</p>
 
-            wan<p>${JSON.stringify(getProperty(obj, intKey).wan)}</p>
-            lan<p>${JSON.stringify(getProperty(obj, intKey).lan)}</p>
+           <h2>Interfaces</h2>
+           <p>${JSON.stringify(getProperty(interfaces))}</p>
+
+           ${getAll(interfaces)}
         </div>
     `;
     document.getElementById('content').insertAdjacentHTML('beforeend', markup);
-   }
+}
