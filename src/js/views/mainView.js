2dc193d8ca38e096326d95c9ceb9b5c3ff745079
Асылов Тимур <asylov@gmail.com>
2019-11-10 10:15:55 +0300

    markup turn to list view

--- src/js/views/mainView.js
+++ src/js/views/mainView.js
@@ -16,28 +16,24 @@ export const clearContent = () => {
 export const renderTemplate = () => {
     const markup = `
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">Support assistant</a>  
+        <a class="navbar-brand" href="#">Support assistant</a>  
 
-            <ul class="nav">
-                <li class="nav-item"
-                data-element="page"
-                >
-                    <a class="nav-link active" href="#" id="info">Information</a>
-                </li>
+        <ul class="nav">
+            <li class="nav-item"
+            data-element="page" >
+                <a class="nav-link active" href="#" id="info">Information</a>
+            </li>
 
-                <li class="nav-item"
-                data-element="page"
-                >                    
-                    <a class="nav-link" href="#" id="rule">Rule checker</a>
-                </li>
+            <li class="nav-item"
+            data-element="page" >                    
+                <a class="nav-link" href="#" id="rule">Rule checker</a>
+            </li>
 
-                <button class="btn btn-outline-primary" type="submit" id="new">
-                Add new config
-                </button>
-            </ul>
+            <button class="btn btn-outline-primary" type="submit" id="new">Add new config</button>
+        </ul>
     </nav>
 
-    <div class="container p-4 col-6">
+    <div class="container p-4">
         <div id="content"></div>
     </div>
  `;
@@ -51,19 +47,13 @@ export const renderInfoTemplate = () => {
     <h2 class="mb-3">Information</h2>
     <div class="row">
         <div class="col">
-            <div class="card rounded" >
-                <div class="card-body" id="gen-info">
-                    <h6 class="card-subtitle mb-3 text-muted">General info</h6>
-                </div>
-            </div>
+            <h6 class="card-subtitle mb-2 text-muted">General info</h6>
+            <ul class="list-group"  id="gen-info"></ul>
         </div>
 
         <div class="col">    
-            <div class="card rounded" >
-                <div class="card-body" id="inf-info">
-                <h6 class="card-subtitle mb-3 text-muted">Interface info</h6>
-                </div>  
-            </div>
+            <h6 class="card-subtitle mb-2 text-muted">Interface info</h6>
+            <ul class="list-group" id="inf-info"></ul>                
         </div> 
     </div>
    `;
@@ -81,12 +71,9 @@ const renderGeneral = info => {
 
         if (keys.indexOf(key) != -1) {
             const markup = `
-                <div>
-                    <h4>${key}</h4>
-                    <p>${value}</p>
-                </div>
-        `;
-            document.getElementById('gen-info').insertAdjacentHTML('beforeend', markup);
+                    <li class="list-group-item">${key} <span class="font-weight-bold">${value}</li>
+            `;
+        document.getElementById('gen-info').insertAdjacentHTML('beforeend', markup);
         }
     }
 }
@@ -97,25 +84,23 @@ const renderInterface = info => {
 
     for (let [key, value] of Object.entries(info.interfaces)) {
         const markup = `
-            <div >
-                <h4>${key.toUpperCase()}</h4>
-            </div>
-
+            <h5 class="mt-3">${key.toUpperCase()}</h5>
         `;
         document.getElementById('inf-info').insertAdjacentHTML('beforeend', markup);
 
         for (let [k, v] of Object.entries(value)) {
             if (v == false) {
-                ''
                 // If need render empty keys
-                /* 
+                /*
                 const markup = `
-                <h5 class="results">${k}</h5><p class="results__value">Empty value</p>`
+                    <li class="list-group-item">${k}: empty value</li>
+                `
                 document.getElementById('inf-info').insertAdjacentHTML('beforeend', markup);
                 */
             } else if (v) {
                 const markup = `
-                    <h5>${k}</h5><p>${v}</p>`
+                    <li class="list-group-item">${k} <span class="font-weight-bold">${v}</span></li>
+                    `
                 document.getElementById('inf-info').insertAdjacentHTML('beforeend', markup);
             }
         }
